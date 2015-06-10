9af2bc6e7f9c27072d07e9cec086c269ae24ecdc
cdholmesatt <ch3347@att.com>
2015-06-09 22:56:17 -0400

    New Commit
    Finally figure out how to use GitHub.  Uploading my p1 project now that
    I have read through the PEP8

--- entertainment.py
+++ entertainment.py
@@ -1,21 +1,19 @@
-
 """ UDACITY FULL STACK WEB DEVELOPER NANODEGREE  - PROJECT 1
-Entertainment.py - python file for Movie info and Movie trailer program.  
------------------------------------------------------------------------------------
-This is the 1st program for the Nanodegree for Full Stack Developer - P1 Submission 
-
+Entertainment.py - python file for Movie info and Movie trailer program.
+------------------------------------------------------------------------------
+This is the 1st program for the Nanodegree for Full Stack Developer - P1
 Available Modules:
 media.py
 entertainment.py
 fresh_cherrytomatoes.py
------------------------------------------------------------------------------------
+------------------------------------------------------------------------------
 This is the documentation for entertainment.py
-# 
+#
 # This program allows a user to view information arranged
 # concerning a pre-built list of several of the developers
 # favorite movies and relevant detail concerning the movie.
 # The detail is fetched from omdb.com using
-# 
+#
 #             'Plot'
 #             'Poster'
 #             'trailer_youtube'
@@ -27,8 +25,7 @@ This is the documentation for entertainment.py
 #             'Actors'
 # You can click the poster image of the movie and it will
 # kick off the official movie trailer found on www.youtube.com
-        
------------------------------------------------------------------------------------"""
+---------------------------------------------------------------------------"""
 
 import fresh_cherrytomatoes
 import media
@@ -49,7 +46,9 @@ godfather = media.Movie("The GodFather")
 pulpfiction = media.Movie("Pulp Fiction")
 killbill = media.Movie("Kill Bill: Vol. 1")
 killbill2 = media.Movie("Kill Bill: Vol. 2")
-movies = [matrix,scarface,thegoodbadugly,independenceday,sanandreas,jurassicworld,goodfellas,trainingday,age_of_ultron,django,godfather,pulpfiction,killbill,killbill2]     
+movies = [matrix, scarface, thegoodbadugly, independenceday, sanandreas,
+          jurassicworld, goodfellas, trainingday, age_of_ultron, django,
+          godfather, pulpfiction, killbill, killbill2]
 fresh_cherrytomatoes.open_movies_page(movies)
 
 
--- media.py
+++ media.py
@@ -1,6 +1,4 @@
 # Import modules
-
-                
 import fresh_cherrytomatoes
 import media
 import os
@@ -14,48 +12,54 @@ import re
 # Store urls values
 omdb = "http://www.omdbapi.com/?t="
 youtube = "http://www.youtube.com/"
-imdb = "http://www.imdb.com/"
+imdb = "http://www.imdb.com/title/"
+
 
 # Movie Class
 class Movie():
-
-    
         # Create/Initiate an object instance of Movie
-        def __init__(self,movie_title):
+        def __init__(self, movie_title):
                 self.title = movie_title
+                # Function definition - Search youtube for movie_title
+                # passed from media.py, return trailer_youtube value
 
-
-                # Function definition - Search youtube for movie_title passed from media.py, return trailer_youtube value
                 def search_youtube():
-                        search_text = self.title+" trailer official"
-                        movie = {"search_query":search_text}
-                        search = urllib.urlopen(youtube+'results'+"?"+urllib.urlencode(movie))
-                        result = search.read()
-                        search.close()       
-                        match = re.search('\"yt-lockup-title\"\>\<a href=\"\/watch\?v\=([\w|\d|-|_]+)\"\s',result)
-                        if match:
-                                match.group(1)
-                                trailer_youtube = youtube+'watch?v='+match.group(1)
-                                return trailer_youtube
+                    search_text = self.title+" trailer official"
+                    movie = {"search_query": search_text}
+                    search = urllib.urlopen(
+                        youtube+'results'+"?"+urllib.urlencode(movie))
+                    result = search.read()
+                    search.close()
+                    search_exp = '\"yt-lockup-title\"\>'
+                    search_exp2 = '\<a href=\"\/watch\?v\=([\w|\d|-|_]+)\"\s'
+                    match = re.search(search_exp+search_exp2, result)
+                    if match:
+                        match.group(1)
+                        trailer_youtube = youtube+'watch?v='+match.group(1)
+                        return trailer_youtube
 
+                # Function definition - Searches omdb for the movie_title
+                # passed from media.py, returns elements associated with the
+                # contents of movie_title via api
+                def search_omdb():
+                    connection = urllib.urlopen(
+                        omdb+self.title+"&y=&plot=short&r=json")
+                    jsoninput = connection.read()
+                    decoded = json.loads(jsoninput)
+                    return decoded
 
-                # Function definition - Searches omdb for the movie_title passed from media.py, returns elements associated with thecontents of movie_title via api
-                def search_omdb():           
-                        connection = urllib.urlopen(omdb+self.title+"&y=&plot=short&r=json")
-                        jsoninput = connection.read()
-                        decoded = json.loads(jsoninput)
-                        return decoded
-
-
-                # Function definition - Uses the movie_title passed from media.py and the imdbID passed from search_omdb() to build the associated imdb url
+                # Function definition - Uses the movie_title passed from
+                # media.py and the imdbID passed from search_omdb() to build
+                # the associated imdb url
                 def search_imdb(decoded):
-                        imdbURL = imdb +'title/'+decoded['imdbID']+"/?ref_=nv_sr_1"
-                        # Plot, Poster, trailer_youtube,Released, Year, Genre,Director, Rated, Actors):           
+                        imdbURL = imdb+decoded['imdbID']+"/?ref_=nv_sr_1"
+                        # Plot, Poster, trailer_youtube, Released,
+                        # Year, Genre, Director, Rated, Actors):
                         return imdbURL
 
-
-                # Function definition - Uses the dictionary passed from search_omdb api
-                def omdb_detail(trailer_youtube,imdbURL,decoded):
+                # Function definition - Uses the dictionary passed
+                # from search_omdb api
+                def omdb_detail(trailer_youtube, imdbURL, decoded):
                         self.storyline = decoded['Plot']
                         self.poster_image_url = decoded['Poster']
                         self.trailer_youtube_url = trailer_youtube
@@ -68,16 +72,11 @@ class Movie():
                         self.imdburl = imdbURL
                         return
 
-
                 # Main function definition
                 def main():
                         movie_trailer = search_youtube()
                         omdb_output = search_omdb()
                         imdb_output = search_imdb(omdb_output)
-                        omdb_detail(movie_trailer,imdb_output,omdb_output)
-
-
+                        omdb_detail(movie_trailer, imdb_output, omdb_output)
                 # Main function
                 main()
-                
-
